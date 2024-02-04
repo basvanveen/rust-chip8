10e2d927b952f7e174711e500d2a48ec6292ba92
Bas van Veen <Bas.van.Veen1@ah.nl>
2024-02-04 17:42:21 +0100

    feat: make display module move logic from main

--- src/display.rs
+++ src/display.rs
@@ -1,16 +1,58 @@
+use sdl2::{video::Window};
+use sdl2::render::Canvas;
+use sdl2::pixels::Color;
+use sdl2::pixels;
+use sdl2::rect::Rect;
+use std::time::Duration;
+
+const SCREEN_WIDTH: u32 = 640;
+const SCREEN_HEIGHT: u32 = 320;
+
 pub struct Display {
-    display: [[u8;8];4]
+    canvas: Canvas<Window>
 }
 
 impl Display{
-    pub fn new() -> Display {
-        Display {
-            display: [[0;8];4]
-        }
+    pub fn new(sdl_context: &sdl2::Sdl) -> Self {
+        let mut i = 0;
+        let video_subsystem = sdl_context.video().unwrap();
+
+        let window = video_subsystem.window("Rust based chip8 emulator @basvanveen", SCREEN_WIDTH, SCREEN_HEIGHT)
+            .position_centered()
+            .opengl()
+            .build()
+            .unwrap();
+      
+        let mut canvas = window.into_canvas().build().unwrap();
+      
+        canvas.set_draw_color(Color::RGB(255, 255, 255));
+        canvas.clear();
+        canvas.present();
+        Display {canvas: canvas }
     }
 
-    pub fn set_display(&mut self, x: u8, y: u8, n:u8){
-        // Jump to location nnn, The interpreter sets the program counter to nnn.
-        println!("x:{:?} y:{:?} n:{:?}",x ,y, n);
+    pub fn draw(&mut self, vram: &[[u8;64];32]){
+        let mut i = 125;
+        i = (i + 1) % 255;
+        self.canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+
+  
+        self.canvas.set_draw_color(Color::RGB(255, 255, 255));
+  
+       //let vram = vram;
+        for (y, row) in vram.iter().enumerate() {
+          for (x, &pixel) in row.iter().enumerate() {
+              let x = (x as u32) * 10;
+              let y = (y as u32) * 10;
+  
+              if pixel == 1 {self.canvas.set_draw_color(pixels::Color::RGB(0, 0, 0));}else{
+                self.canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+              }
+  
+              let _ = self.canvas
+                  .fill_rect(Rect::new(x as i32, y as i32, 10, 10));
+          }
+      }
+        self.canvas.present();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,18 @@ use crate::chip8::Chip8;
 mod memory;
 mod chip8;
 mod cpu;
+mod input;
+mod display;
 
-
+use display::Display;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
-use sdl2::pixels;
 use std::time::Duration;
-
-use sdl2::rect::Rect;
-
-
-const SCREEN_WIDTH: u32 = 640;
-const SCREEN_HEIGHT: u32 = 320;
-
 
 
 fn main() {
   let sdl_context = sdl2::init().unwrap();
-  let video_subsystem = sdl_context.video().unwrap();
-
-  let window = video_subsystem.window("Rust based chip8 emulator @basvanveen", SCREEN_WIDTH, SCREEN_HEIGHT)
-      .position_centered()
-      .opengl()
-      .build()
-      .unwrap();
-
-  let mut canvas = window.into_canvas().build().unwrap();
-
-  canvas.set_draw_color(Color::RGB(255, 255, 255));
-  canvas.clear();
-  canvas.present();
+  let mut display = Display::new(&sdl_context);
 
   let mut rng = rand::thread_rng();
 
@@ -102,32 +83,12 @@ fn main() {
               _ => {}
           }
       }
-
-      i = (i + 1) % 255;
-      canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-
-      // Run Instructions on Chip8 VM
-      let output = chip8.run_instruction();
-
-      canvas.set_draw_color(Color::RGB(255, 255, 255));
-
-      let vram = output.vram;
-      for (y, row) in vram.iter().enumerate() {
-        for (x, &pixel) in row.iter().enumerate() {
-            let x = (x as u32) * 10;
-            let y = (y as u32) * 10;
-
-            if pixel == 1 {canvas.set_draw_color(pixels::Color::RGB(0, 0, 0));}else{
-              canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-            }
-
-            let _ = canvas
-                .fill_rect(Rect::new(x as i32, y as i32, 10, 10));
-        }
-    }
-      canvas.present();
-      // limit FPS if needed
-      ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 20));
+        // Run Instructions on Chip8 VM
+        let output = chip8.run_instruction();
+        // Draw Display
+        display.draw(output.vram);
+        // limit FPS if needed
+        //::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 20));
   }
 
 }
